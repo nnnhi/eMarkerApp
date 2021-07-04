d59927ecc700ba045258e0aad1de4e780d4ac9ae
Nam Nhi Nguyen <namnhihg@gmail.com>
2021-07-04 15:24:44 -0400

    Fix wrong path

--- src/app.config.js
+++ src/app.config.js
@@ -1,5 +1,5 @@
 export const youtubeThumbnail = "http://img.youtube.com/vi/{video-id}/hqdefault.jpg";
-export const API_URL = process.env.VUE_APP_API_URL || "https://localhost:5001/api";
+export const API_URL = process.env.VUE_APP_API_URL || "https://localhost:5001";
 export const youtubeAPI = "https://youtube.googleapis.com/youtube/v3/videos"
 
 
